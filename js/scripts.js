f0c51ad352af26846532698c66f3f8aaa873bc59
Maureen Roble <moroble@gmail.com>
2016-02-13 17:32:50 -0500

    add more contact form code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $("#contactForm").validator().on("submit", function (event) {
     if (event.isDefaultPrevented()) {
+    	formError();
+   	submitMSG(false, "Did you fill in the form properly?");
    }else { 
-//everything looks good!
+
 	event.preventDefault();
    	submitForm();
+   	
+   }
 });
 
 function submitForm(){
@@ -25,4 +29,22 @@ function submitForm(){
 }
 function formSuccess(){
     $( "#msgSubmit" ).removeClass( "hidden" );
+    $("#contactForm")[0].reset();
+	submitMSG(true, "Message Submitted!")
+}
+
+function formError(){
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+        var msgClasses;
+    if(valid){
+        msgClasses = "h3 text-center tada animated text-success";
+    } else {
+        msgClasses = "h3 text-center text-danger";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
 }
